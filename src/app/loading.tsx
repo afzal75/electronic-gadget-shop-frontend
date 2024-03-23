81b9b443e1cad2196f3a4022e4898b7fb83e2c3b
afzal75 <afzalbhuiyan16@gmail.com>
2024-03-23 14:15:33 +0600

    loading page and not-found page created succesfully

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div className='w-10 h-10 border-8 border-dashed rounded-full animate-spin mt-5 border-green-400'></div>
+        </div>
+    )
+}
+
+export default Loading
