095f5561173d858f2f409dd63e036b425e0018cc
afzal75 <afzalbhuiyan16@gmail.com>
2024-03-23 14:08:27 +0600

    project setup successfully done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>THis is</h1>
+      <Button>Submit</Button>
+    </div>
+  );
+}
